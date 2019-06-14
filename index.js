f87a396dcf23d4a652a6f7ead553fc9e7991aadc
Luiz.Machado <luiz.machado@mac234512.local>
2019-06-13 23:43:00 -0300

    log with paths

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 "use strict";
-
-const util = require("util");
-const exec = util.promisify(require("child_process").exec);
+const paths = require("path");
+const { execSync } = require("child_process");
 
 class MaestroPlugin {
   constructor(serverless, options) {
     this.serverless = serverless;
     this.options = options;
     this.maestro = serverless.service.custom.maestro;
+    this.serverlessLog = serverless.cli.log.bind(serverless.cli);
+
+    this.stdin = process.stdin;
+    this.stdout = process.stdout;
+    this.stderr = process.stderr;
 
     this.commands = {
       maestro: {
-        usage:
-          "Need a maestro for your serverless orchest ? type maestro deploy",
+        usage: 'Just run "sls maestro play" ',
         commands: {
-          deploy: {
-            lifecycleEvents: ["execute"]
+          play: {
+            lifecycleEvents: ["run"],
+            usage: "Just play"
           }
         }
       }
     };
 
     this.hooks = {
-      "maestro:deploy:execute": this.deploy.bind(this)
+      "maestro:play:run": this.run.bind(this)
     };
   }
 
@@ -31,48 +35,46 @@ class MaestroPlugin {
     simpleGit.clone(repoPath);
     this.serverless.cli.log("cloned");
   }
+  cloneProject() {
+    const simpleGit = require("simple-git")(__dirname);
+    simpleGit.clone(project.git);
+    let start = project.git.lastIndexOf("/") + 1;
+    let final = project.git.lastIndexOf(".");
+    let repoName = project.git.slice(start, final);
+    console.log(repoName);
+    project.path = __dirname + "/" + repoName;
+    console.log(project.path);
+  }
 
-  deploy() {
+  run() {
     this.maestro.forEach(object => {
       Object.keys(object).forEach(key => {
         let project = object[key];
         project.name = key;
         if (project.git) {
-          const simpleGit = require("simple-git")(__dirname);
-          simpleGit.clone(project.git);
-          let start = project.git.lastIndexOf("/") + 1;
-          let final = project.git.lastIndexOf(".");
-          let repoName = project.git.slice(start, final);
-          console.log(repoName);
-          project.path = __dirname + "/" + repoName;
-          console.log(project.path);
+          cloneProject();
         }
-        this.execute("package", project);
+        this.runCommand(project);
       });
     });
   }
 
-  log(stringLog) {
-    this.serverless.cli.log(stringLog);
-  }
+  runCommand(project) {
+    const { name, path, command } = project;
+    this.serverlessLog(
+      `##############################################################`
+    );
+    this.serverlessLog(`Maestro is playing "${command}" on ${name}`);
 
-  async execute(_command, project) {
-    const options = {
-      cwd: project.path
-    };
+    execSync(`${command} -v`, {
+      stdio: [this.stdin, this.stdout, this.stderr],
+      cwd: paths.resolve(path)
+    });
 
-    this.log(`Started sls ${_command} on ${project.name}`);
-
-    // const { stdout, stderr } = await exec(`ls`);
-
-    const { stdout, stderr } = await exec("find . -type f | wc -l");
-    if (stderr) {
-      this.log(`stderr: ${stderr}`);
-    }
-
-    this.log(`Working dir ${project.path}:${_command}:${stdout}`);
-
-    this.log(`Finished sls ${_command} on ${Object.keys(project)[0]}`);
+    this.serverlessLog(`Maestro ended "${command}" on ${name}`);
+    this.serverlessLog(
+      `##############################################################`
+    );
   }
 }
 
